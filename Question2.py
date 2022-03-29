335d28485c0779d6432c07a7133fafc8d8310788
DnQk <2102593@sit.singaporetech.edu.sg>
2022-03-29 11:22:55 +0800

    Week 10 Lab

---
+++ Question2.py
@@ -0,0 +1,4 @@
+x = float(input("\nEnter x value: ")) # Input value x
+y = float(input("\nEnter y value: ")) # Input value y
+z = float((x + y)/2) # Calculate average
+print("\nAverage of ", x, " and ", y, " is ", z) # Print average of x and y5
